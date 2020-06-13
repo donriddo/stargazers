b897bae135dcca5b696283777e343084748e6c19
Ridwan Taiwo <donriddo@gmail.com>
2020-06-13 02:26:35 +0100

    implement the Block.validate method

--- src/block.js
+++ src/block.js
@@ -15,14 +15,14 @@ const hex2ascii = require('hex2ascii');
 class Block {
 
     // Constructor - argument data will be the object containing the transaction data
-	constructor(data){
-		this.hash = null;                                           // Hash of the block
-		this.height = 0;                                            // Block Height (consecutive number of each block)
-		this.body = Buffer(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
-		this.time = 0;                                              // Timestamp for the Block creation
-		this.previousBlockHash = null;                              // Reference to the previous Block Hash
+    constructor(data) {
+        this.hash = null;                                           // Hash of the block
+        this.height = 0;                                            // Block Height (consecutive number of each block)
+        this.body = Buffer(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+        this.time = 0;                                              // Timestamp for the Block creation
+        this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
-    
+
     /**
      *  validate() method will validate if the block has been tampered or not.
      *  Been tampered means that someone from outside the application tried to change
@@ -38,14 +38,22 @@ class Block {
     validate() {
         let self = this;
         return new Promise((resolve, reject) => {
-            // Save in auxiliary variable the current block hash
-                                            
-            // Recalculate the hash of the Block
-            // Comparing if the hashes changed
-            // Returning the Block is not valid
-            
-            // Returning the Block is valid
-
+            try {
+                // Save in auxiliary variable the current block hash
+                const currentBlockHash = self.hash;
+                // Recalculate the hash of the Block
+                const hash = SHA256(JSON.stringify(self)).toString();
+                // Comparing if the hashes changed
+                if (hash !== currentBlockHash) {
+                    // Returning the Block is not valid
+                    resolve(false);
+                } else {
+                    // Returning the Block is valid
+                    resolve(true);
+                }
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 
